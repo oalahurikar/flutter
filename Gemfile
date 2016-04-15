ea101852fe60b1e7e2048e5c88117120fa76407a
oalahurikar <oalahuri@mtu.edu>
2016-04-15 12:49:13 +0000

    Before Mailboxer

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'mailboxer'
 gem 'devise'
 gem 'bootstrap-sass'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#index'
+  root 'pages#home'
   resources :posts
   
   get '/user/:id' => 'pages#profile'
